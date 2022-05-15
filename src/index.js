7fd22cf7fe6011ade1cefa5aeece5ab6eb7a413d
ElMigu17 <rgomiguel17@gmail.com>
2022-05-15 18:32:00 -0300

    organizando

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Profile from './Profile';
+import Profile from './Profile/Profile';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
