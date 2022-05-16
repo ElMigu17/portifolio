5147e360a10c81d666acfcd69df101cb5e43b748
ElMigu17 <rgomiguel17@gmail.com>
2022-05-16 01:02:11 -0300

    colocando o contato na página

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Profile from './Profile/Profile';
+import Contact from './Contact/Contact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Profile/>
+    <Contact/>
   </React.StrictMode>
 );
 
