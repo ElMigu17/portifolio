893779354f2f0829da9be2cde1cf33c539fe7f47
ElMigu17 <rgomiguel17@gmail.com>
2022-05-17 11:30:39 -0300

    adicionado tela de loading

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,5 +1,6 @@
 import Title from '../Title/Title';
 import acm from '../assets/adams_creation_minimalist.png'
+import load from '../assets/load.png'
 import "./Contact.scss";
 import apiKey from '../emailkey.js';
 import emailjs from '@emailjs/browser';
@@ -7,21 +8,28 @@ import React, { useRef } from 'react';
 
 function Contact() {
   const form = useRef();
-
+  let sent = false;
   const handleSubmit = (e) => {
-    e.preventDefault(); 
-    console.log(apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
-    emailjs.init("I2rhV_tvDUEQO11ZG");
-    emailjs.sendForm(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
-    
-    .then((result) => {
-      alert("Message Sent");
-    },
-    (error) => {
-      alert("An error occurred, Please try again");
-      console.log(error.text);
-
-    });
+    if(!sent){
+      sent = true;
+      let element = document.getElementById("loading-div");
+      element.style.display = "flex";
+      e.preventDefault(); 
+      console.log(apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
+      emailjs.init("I2rhV_tvDUEQO11ZG");
+      emailjs.sendForm(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
+      .then((result) => {
+        alert("Message Sent");
+      },
+      (error) => {
+        alert("An error occurred, Please try again");
+        console.log(error.text);
+      })
+      .finally(() => {
+        element.style.display = "none";
+        sent = false;  
+      })
+    }
   };
 
   return (
@@ -36,7 +44,8 @@ function Contact() {
           <textarea className='message' name='message' ></textarea>
           <h4 className='contact-question'> Could I contact you back? How? </h4>
           <input className='contact-addres' name="contactBack"></input>
-          <input className='submit-button' type="submit" value="Send" ></input>
+          <input id='submit-email' className='submit-button' type="submit" value="Send" ></input>
+          <div id='loading-div'><img id='image-loading' src={load} alt="loading animation" /></div>
         </form>
 
         <div className='pos-img'>
